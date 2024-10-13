7d5d99eec5f5216195f8938c233b1097cb0b1f56
Dimitris Mor <dimitris_morakis@hotmail.com>
2024-10-13 23:54:10 +0300

    Initial commit

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   MapContainer,
   TileLayer,
@@ -14,7 +14,9 @@ import "leaflet-draw/dist/leaflet.draw.css";
 import L from "leaflet";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import { CRS } from "leaflet";
-import { Circle } from "react-leaflet";
+import { Circle, Rectangle, Pane } from "react-leaflet";
+import { SVGOverlay, LayerGroup, Tooltip, LayersControl } from "react-leaflet";
+import { LatLngTuple } from "leaflet";
 
 const customMarkerIcon = new L.Icon({
   iconUrl: process.env.PUBLIC_URL + "/map-marker.png",
@@ -30,6 +32,22 @@ const DrawingMap: React.FC = () => {
   const athensPosition: [number, number] = [37.9838, 23.7275];
 
   const fillBlueOptions = { fillColor: "blue" };
+
+  const overlayBounds: L.LatLngBoundsLiteral = [
+    [51.48, -0.1],
+    [51.51, -0.05],
+  ];
+
+  const center: LatLngTuple = [51.505, -0.09];
+
+  const outer: L.LatLngBoundsLiteral = [
+    [50.505, -29.09],
+    [52.505, 29.09],
+  ]
+  const inner: L.LatLngBoundsLiteral = [
+    [49.505, -2.09],
+    [53.505, 2.09],
+  ]
 
   const handleCreate = (e: any) => {
     const type = e.layerType;
@@ -60,6 +78,25 @@ const DrawingMap: React.FC = () => {
   //   )
   // }
 
+  function BlinkingPane() {
+    const [render, setRender] = useState(true)
+    const timerRef: any = useRef()
+    useEffect(() => {
+      timerRef.current = setInterval(() => {
+        setRender((r) => !r)
+      }, 5000)
+      return () => {
+        clearInterval(timerRef.current)
+      }
+    }, [])
+  
+    return render ? (
+      <Pane name="cyan-rectangle" style={{ zIndex: 500 }}>
+        <Rectangle bounds={outer} pathOptions={{ color: 'cyan' }} />
+      </Pane>
+    ) : null
+  }
+
   return (
     <MapContainer
       center={greeceCenter}
@@ -87,7 +124,21 @@ const DrawingMap: React.FC = () => {
         pathOptions={fillBlueOptions}
         radius={100200}
       />
-
+      <Circle
+        center={athensPosition}
+        pathOptions={fillBlueOptions}
+        radius={1200}
+      >
+        {" "}
+        <Tooltip>Tooltip test</Tooltip>
+      </Circle>
+      {/* <SVGOverlay attributes={{ stroke: "red" }} bounds={overlayBounds}>
+        <rect x="0" y="0" width="100%" height="100%" fill="blue" />
+        <circle r="5" cx="10" cy="10" fill="red" />
+        <text x="50%" y="50%" stroke="white">
+          text
+        </text>
+      </SVGOverlay> */}
       <FeatureGroup>
         <EditControl
           position="topright"
@@ -100,6 +151,45 @@ const DrawingMap: React.FC = () => {
           }}
         />
       </FeatureGroup>
+      <LayersControl position="topright">
+        <LayersControl.Overlay name="Marker with popup">
+          <Marker position={center}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </LayersControl.Overlay>
+        <LayersControl.Overlay checked name="Layer group with circles">
+          <LayerGroup>
+            <Circle
+              center={center}
+              pathOptions={{ fillColor: "blue" }}
+              radius={200}
+            />
+            <Circle
+              center={center}
+              pathOptions={{ fillColor: "red" }}
+              radius={100}
+              stroke={false}
+            />
+            <LayerGroup>
+              <Circle
+                center={[51.51, -0.08]}
+                pathOptions={{ color: "green", fillColor: "green" }}
+                radius={100}
+              />
+            </LayerGroup>
+          </LayerGroup>
+        </LayersControl.Overlay>
+      </LayersControl>
+
+      {/* <BlinkingPane /> */}
+    <Pane name="yellow-rectangle" style={{ zIndex: 499 }}>
+      <Rectangle bounds={inner} pathOptions={{ color: 'yellow' }} />
+      <Pane name="purple-rectangle">
+        <Rectangle bounds={outer} pathOptions={{ color: 'purple' }} />
+      </Pane>
+    </Pane>
     </MapContainer>
   );
 };
